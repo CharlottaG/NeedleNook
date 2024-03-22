2a9142dd094c01f15ebe155fd8b45bbcf6cc62d0
CharlottaG <DELETED-1691214276921>
2024-03-22 12:35:25 +0000

    Add Comment functionality to admin panel

--- patterns/admin.py
+++ patterns/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Pattern
+from .models import Pattern, Comment
 
 # Register your models here.
 admin.site.register(Pattern)
+admin.site.register(Comment)
